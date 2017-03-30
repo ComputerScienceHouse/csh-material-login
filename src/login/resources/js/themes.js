83b2cc39573a66ff9c7df2283563f79197d008ee
Steven Mirabito <steven@stevenmirabito.com>
2017-03-30 02:54:24 -0700

    Add Raven client and Redux middleware

--- src/login/resources/js/themes.js
+++ src/login/resources/js/themes.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { compose, createStore } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import persistState from 'redux-localstorage';
+import Raven from "raven-js";
+import createRavenMiddleware from "raven-for-redux";
 
 import Themes from './components/Themes';
 import reducer from './reducers';
+
+const RAVEN_DSN = 'https://5672ee5945634745ad84d4a8470d08bc@sentry.io/153480';
+Raven.config(RAVEN_DSN).install();
 
 const enhancer = compose(
   persistState(undefined, {
     key: 'loginThemes'
   }),
+  applyMiddleware(
+      createRavenMiddleware(Raven),
+  ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
