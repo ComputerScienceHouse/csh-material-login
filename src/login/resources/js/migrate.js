56cacebcefba12c90f3dd7c38cd0d086e3f1be03
Steven Mirabito <steven@stevenmirabito.com>
2018-01-23 02:34:43 -0500

    Update form binding for migration

--- src/login/resources/js/migrate.js
+++ src/login/resources/js/migrate.js
@@ -22,25 +22,23 @@ function inject() {
   try {
     const loginForm = document.querySelector('form[action*="login-actions/authenticate"');
     const submitBtn = loginForm.querySelector('input[type="submit"]');
-    submitBtn.addEventListener('click', function() {
-      submitBtn.disabled = true;
-      const usernameField = loginForm.querySelector('input[name="username"]');
-      const passwordField = loginForm.querySelector('input[name="password"]');
+    const usernameField = loginForm.querySelector('input[name="username"]');
+    const passwordField = loginForm.querySelector('input[name="password"]');
 
-      if (usernameField && passwordField) {
+    if (usernameField && passwordField) {
+      submitBtn.addEventListener('click', function() {
+        submitBtn.disabled = true;
+
         migrate(usernameField.value, passwordField.value)
           .then(() => {
             loginForm.submit();
           });
 
         return false;
-      }
-
-      return true;
-    });
+      });
+    }
   } catch (e) {
     console.error('[MIGRATE] Failed to bind to form:', e);
-    return true;
   }
 }
 
