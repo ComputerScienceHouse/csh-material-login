da6af0fa579cf48d3576d2a87a4a24d38539767d
Shawn Chowdhury <shawn.rc0@gmail.com>
2018-04-05 09:13:53 -0400

    refactors some common logic in Logo

--- src/login/resources/js/components/Logo.js
+++ src/login/resources/js/components/Logo.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { renderToString } from 'react-dom/lib/ReactServerRendering';
 
 const LOGIN_BOX_ID = 'login-box';
+const LOGO_CLASS = 'logo';
 
 const fetchRawSvg = (url) => {
   const request = new XMLHttpRequest();
@@ -10,31 +11,27 @@ const fetchRawSvg = (url) => {
   return request.responseText;
 };
 
+const insertHtmlAtMountpoint = (html) =>
+  document.getElementById(LOGIN_BOX_ID).insertAdjacentHTML('afterbegin', html);
+
 const Logo = (props) => {
-  const existing = document.querySelector('.logo');
+  const existing = document.querySelector(`.${LOGO_CLASS}`);
   if (existing !== null) existing.remove();
 
   if (props.src.endsWith('svg')) {
-    const elementHtml = fetchRawSvg(props.src);
-    document.getElementById(LOGIN_BOX_ID).insertAdjacentHTML(
-      'afterbegin',
-      elementHtml
-    );
+    insertHtmlAtMountpoint(fetchRawSvg(props.src));
     const inlined = document.querySelector('svg');
     inlined.removeAttribute('height');
-    inlined.setAttribute('class', 'logo');
+    inlined.setAttribute('class', LOGO_CLASS);
   } else {
-    const img = <img src={props.src} className={'logo'} />;
-    document.getElementById(LOGIN_BOX_ID).insertAdjacentHTML(
-      'afterbegin',
-      renderToString(img)
-    );
+    const img = <img src={props.src} className={LOGO_CLASS} />;
+    insertHtmlAtMountpoint(renderToString(img))
   }
   return null;
 };
 
 Logo.propTypes = {
-  src: PropTypes.string,
+  src: PropTypes.string.isRequired,
 };
 
 export default Logo;
